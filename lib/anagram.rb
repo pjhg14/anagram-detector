27d02758c6a0c3a33bdc01dc9488172565c33ff4
pj <pjhg14@gmail.com>
2021-04-27 22:58:39 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :match
+
+    def initialize(match)
+        @match = match
+    end
+
+    def match(phrase)
+        # phrase should be an array
+        phrase.filter do |word|
+            word.split("").sort == @match.split("").sort
+        end
+    end
+end
